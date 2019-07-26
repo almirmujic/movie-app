f129c20cc8fe0a497816643391896f6e62072671
Almir <contact.almir@gmail.com>
2019-07-26 11:45:32 -0500

    View button onClick relocates user to the movie url

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -15,8 +15,33 @@ const Card = styled.div`
   }
 `;
 
+const Img = styled.img`
+  border-radius: 15px;
+  border: 5px solid #01d277;
+  max-height: 278px;
+`;
+
+const Button = styled.button`
+  background-color: #01d277;
+  color: #fff;
+  font-size: 20px;
+  border: none;
+  border-radius: 15px;
+  padding: 4px 12px 4px 12px;
+  transition: .3s
+  &:hover {
+    transform: scale(1.1);
+    cursor: pointer;
+  }
+`;
+
 export default function MovieCard(props) {
   const imgUrl = 'https://image.tmdb.org/t/p/w185';
+
+  const onView = e => {
+    const url = 'https://www.themoviedb.org/movie/' + props.id;
+    window.location.href = url;
+  };
 
   const imgProvided = props.imgExtention
     ? `${imgUrl}/${props.imgExtention}`
@@ -24,11 +49,11 @@ export default function MovieCard(props) {
 
   return (
     <Card>
-      <img src={imgProvided} alt={props.title} />
+      <Img src={imgProvided} alt={props.title} />
       <div>
         <h2>{props.title}</h2>
         <p>{props.overview}</p>
-        <input type="button">View</input>
+        <Button onClick={onView}>View</Button>
       </div>
     </Card>
   );
